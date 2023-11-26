e7d1ae5a9d0d5e627137aed8b2749758909dbea0
ShreyasNimkar <nimkarshreyas22@gmail.com>
2023-11-26 17:54:58 +0530

    fix:cors issue fix 2

--- src/server.js
+++ src/server.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
